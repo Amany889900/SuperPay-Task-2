5956c3d58fd4819cfb8dd8b49b006a764ee3aca4
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-17 15:54:43 +0300

    cardNum

--- Form/src/app/components/form/form.component.ts
+++ Form/src/app/components/form/form.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  isValid(digits:string) {
+    let sum = 0;
 
+    for (let i = 0; i < digits.length; i++) {
+        let cardNum = parseInt(digits[i]);
+
+        if ((digits.length - i) % 2 === 0) {
+            cardNum = cardNum * 2;
+
+            if (cardNum > 9) {
+                cardNum = cardNum - 9;
+            }
+        }
+
+        sum += cardNum;
+    }
+
+    return sum % 10 === 0;
+
+}
 }
