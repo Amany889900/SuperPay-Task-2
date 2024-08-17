a75491fcbaf69b14ad5d5e2b5a805f2d1dda5841
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-17 17:25:50 +0300

    #cardValidation

--- Form/src/app/components/form/form.component.ts
+++ Form/src/app/components/form/form.component.ts
@@ -6,11 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  formData = {
+    cardNum: '',
+  };
+formatedInput(digits:string){
+  const formattedNumber = digits.replace(/(\d{4})(?=\d)/g, '$1 ');
+  this.formData.cardNum = formattedNumber.trim();
+}
+
   isValid(digits:string) {
     let sum = 0;
 
     for (let i = 0; i < digits.length; i++) {
         let cardNum = parseInt(digits[i]);
+
+        if (isNaN(cardNum)) {
+          continue;
+        }
 
         if ((digits.length - i) % 2 === 0) {
             cardNum = cardNum * 2;
